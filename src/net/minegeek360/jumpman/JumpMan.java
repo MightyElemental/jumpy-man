f834a5550aa9a51ec8c0e9f9f3e0a3c1cce04519
WolfgangTS <lobo.noe@gmail.com>
2015-07-24 23:53:26 +0100

    Update

--- src/net/minegeek360/jumpman/JumpMan.java
+++ src/net/minegeek360/jumpman/JumpMan.java
@@ -1,9 +1,24 @@
 package net.minegeek360.jumpman;
 
-public class JumpMan {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class JumpMan extends StateBasedGame {
+
+	public JumpMan(String name)
+	{
+		super(name);
+	}
 
 	public static void main(String[] args) {
 		
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException
+	{
+
 	}
 
 }
