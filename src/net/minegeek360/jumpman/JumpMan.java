fccd79cfb60db0eb09329fd33a145e6a34db017a
MightyElemental <minebox30@gmail.com>
2015-07-25 00:04:46 +0100

    More setup

--- src/net/minegeek360/jumpman/JumpMan.java
+++ src/net/minegeek360/jumpman/JumpMan.java
@@ -6,15 +6,17 @@ import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class JumpMan extends StateBasedGame {
+	
+	public static final String GAME_VERSION = "0.0.0";
 
-	public static final String GAME_NAME = "Slick Test";
+	public static final String GAME_NAME = "The Adventures Of Jumpy Man | Version "+GAME_VERSION;
 
-	public static final int menu = 0;
-	public static final int play = 1;
+	public static final int STATE_MENU = 0;
+	public static final int STATE_PLAY = 1;
 
 	public static final int[] commonResolutions16_9 = { 1280, 1600, 1920, 2048 };
 
-	public static final int width = commonResolutions16_9[1];
+	public static final int width = commonResolutions16_9[2];
 
 	public JumpMan(String name) {
 		super(name);
@@ -24,7 +26,7 @@ public class JumpMan extends StateBasedGame {
 		AppGameContainer appGc;
 		try {
 			appGc = new AppGameContainer(new JumpMan(GAME_NAME));
-			appGc.setDisplayMode(width, (int) (width / 16.0 * 9.0), true);
+			appGc.setDisplayMode(width, (int) (width / 16.0 * 9.0), false);
 			appGc.setTargetFrameRate(60);
 			appGc.start();
 		} catch (SlickException e) {
