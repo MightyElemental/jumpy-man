9d57f3b27274005bb4684991aecc0778e324cec0
MightyElemental <minebox30@gmail.com>
2015-07-25 19:37:58 +0100

    More edits

---
+++ src/net/minegeek360/jumpman/world/World.java
@@ -0,0 +1,19 @@
+package net.minegeek360.jumpman.world;
+
+import java.util.ArrayList;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.state.StateBasedGame;
+
+import net.minegeek360.jumpman.entities.Entity;
+import net.minegeek360.jumpman.entities.EntityLiving;
+
+public class World {
+
+	public ArrayList<Entity> entities = new ArrayList<Entity>();
+	
+	public void init(GameContainer gc, StateBasedGame sbg){
+		entities.add(new EntityLiving("That thing",50,50).setMoveSpeed(50));
+	}
+
+}
