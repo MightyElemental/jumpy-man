4e47ac968884c02280e8bde652455c3c20ef1ce5
MightyElemental <minebox30@gmail.com>
2015-07-29 22:57:56 +0100

    asdf

--- src/net/minegeek360/jumpman/world/World.java
+++ src/net/minegeek360/jumpman/world/World.java
@@ -1,6 +1,7 @@
 package net.minegeek360.jumpman.world;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.state.StateBasedGame;
@@ -14,6 +15,8 @@ public class World {
 	public ArrayList<Entity>			entities	= new ArrayList<Entity>();
 	public ArrayList<EntityParticle>	particles	= new ArrayList<EntityParticle>();
 	public ArrayList<WorldMap>			mapArray	= new ArrayList<WorldMap>();
+
+	public Random rand = new Random();
 
 	/** The velocity to be added every tick the entities are not on the ground */
 	public float gravity = 0.6f; // should be 9.8m/s/s (9.8m per second for every second in the air)
