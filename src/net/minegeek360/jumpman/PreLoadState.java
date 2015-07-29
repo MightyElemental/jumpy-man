faa2ee40f6b413f49c1a11354d7263e4ba1f8383
WolfgangTS <lobo.noe@gmail.com>
2015-07-29 14:05:27 +0100

    stuff and things += ture

--- src/net/minegeek360/jumpman/PreLoadState.java
+++ src/net/minegeek360/jumpman/PreLoadState.java
@@ -6,6 +6,7 @@ import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
@@ -60,6 +61,11 @@ public class PreLoadState extends BasicGameState {
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) throws SlickException {
+		if(gc.getInput().isKeyDown(Input.KEY_SPACE))
+		{
+			ticks+=500;
+		}
+		
 		ticks += delta / 17f;
 		if (ticks > 150) {
 			alpha -= 0.01f;
