65b3f24dfd6311dd7f29f746836b9c03b3a5f3be
MightyElemental <minebox30@gmail.com>
2015-07-30 15:48:42 +0100

    player controls

--- src/net/minegeek360/jumpman/entities/EntityPlayer.java
+++ src/net/minegeek360/jumpman/entities/EntityPlayer.java
@@ -9,7 +9,8 @@ import net.minegeek360.jumpman.world.World;
 
 public class EntityPlayer extends EntityLiving {
 
-	int[] playerCont = { Input.KEY_D, Input.KEY_A, Input.KEY_K, Input.KEY_L };
+	int[]	playerCont		= { Input.KEY_D, Input.KEY_A, Input.KEY_W, Input.KEY_SPACE };
+	int[]	playerAltCont	= { Input.KEY_D, Input.KEY_A, Input.KEY_K, Input.KEY_L };
 
 	public EntityPlayer( World worldObj ) {
 		super("Player", 35, 50, worldObj);
@@ -25,26 +26,26 @@ public class EntityPlayer extends EntityLiving {
 
 	public void createParticle() {
 		if (!this.isInAir && worldObj.rand.nextInt(5) == 0)
-			worldObj.particles.add(new EntityStepParticle(this.getPosX() + this.width / 2, this.getPosY() + this.height -3, worldObj));
+			worldObj.particles.add(new EntityStepParticle(this.getPosX() + this.width / 2, this.getPosY() + this.height - 3, worldObj));
 	}
 
 	public void update(GameContainer gc, StateBasedGame sbg, int delta) {
 		super.update(gc, sbg, delta);
 		Input input = gc.getInput();
-		if (input.isKeyDown(playerCont[0])) {
+		if (input.isKeyDown(playerCont[0]) || input.isKeyDown(playerAltCont[0])) {
 			if (this.canGoRight) {
 				this.moveRight();
 				createParticle();
 			}
 		}
-		if (input.isKeyDown(playerCont[1])) {
+		if (input.isKeyDown(playerCont[1]) || input.isKeyDown(playerAltCont[1])) {
 			if (this.canGoLeft) {
 				this.moveLeft();
 				createParticle();
 
 			}
 		}
-		if (input.isKeyDown(playerCont[2])) {
+		if (input.isKeyDown(playerCont[2]) || input.isKeyDown(playerAltCont[2])) {
 			this.jump();
 		}
 	}
